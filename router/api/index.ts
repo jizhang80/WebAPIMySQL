d81cc9c567fc337c1e7f3fba8d8afe0255ad9f27
Ji Zhang <jizhang@JiMBP.local>
2024-02-07 20:17:48 -0500

    add post api

--- router/api/index.ts
+++ router/api/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import userRoutes from './userRoutes';
+import postRoutes from './postRoutes';
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 export default router;
--- router/api/postRoutes.ts
+++ router/api/postRoutes.ts
@@ -4,11 +4,41 @@ const router = express.Router();
 
 // add post
 router.post('/', async (req, res) => {
-	//get all the posts
+	const data = {
+		title: req.body.title,
+		content: req.body.content,
+		published: true,
+		authorId: req.body.authorId,
+	}
+	try {
+		const post = await prisma.post.create({
+			data,
+		});
+		//check return value
+		if (!post) {
+	      res
+	        .status(400)
+	        .json({ message: 'post create error.' });
+	      return;
+	    }
 
-	// get posts by id
+	    res.status(200).json({post: post, message: "new post created."});
+	} catch (err) {
+		res.status(400).json(err)
+	}
+});
 
-	// add post
+//get all the posts
+router.get('/', async (req, res) => {
+	const posts = await prisma.post.findMany();
+	res.status(200).json({posts: posts, message: "get all the posts."});
+});
 
-	// add comment
-};
+// get posts by id
+
+// add post
+
+// add comment
+
+
+export default router;
